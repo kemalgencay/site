f11aa24c767003080fc0fede6502e9a21dbce01b
kemalgencay <kemalgencay@gmail.com>
2023-04-11 13:23:37 +0300

    re

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,1 +1,9 @@
-import { ComponentChildren } from "preact";
+export function Footer() {
+    return (
+      <footer>
+        <hr class="my-2" />
+        <p>I am a footer</p>
+      </footer>
+    );
+  }
+  
