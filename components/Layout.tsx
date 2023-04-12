60a701b40a9e2172d7d4ec514a33cedda0131718
kemalgencay <kemalgencay@gmail.com>
2023-04-12 15:41:30 +0300

    kemalblog.com added

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,5 @@
 import { tw } from "twind";
-import Footer from "./Footer.tsx";
+import { Footer } from "./Footer.tsx";
 
 export default function layouts({ children }: any) {
     return (
@@ -9,7 +9,7 @@ export default function layouts({ children }: any) {
             <nav class='tw w-full px-4 py-3 bg-yellow-800 text-white mb-10'>
                 <a class='tw mx-4' href="/">Ana sayfa</a>
                 <a class='tw mx-4' href="/hakkimda">Hakkımda</a>
-                <a class='tw mx-4' href="https://kemalgencay-blog.deno.dev" target="blank">Yazılar</a>
+                <a class='tw mx-4' href="https://kemalblog.com" target="blank">Yazılar</a>
             </nav>
             {children}
             <Footer />
